98b629e56cb0cc30975eb049f9dc6ce037139d3d
AJ <79541631+TheAJManagement@users.noreply.github.com>
2021-06-03 15:08:22 +0100

    Delete button & README.md

--- assets/js/appendToTable.js
+++ assets/js/appendToTable.js
@@ -6,6 +6,9 @@ const table = document.getElementById('table-body');
 
 export function appendToTable(arr) {
     const row = document.createElement('tr');
+    const button = document.createElement('button');
+    button.setAttribute('onclick', 'deleteRow(this);');
+    button.textContent = '-';
     let date = arr[1].split("-").reverse().join("/"); // Converts date to dd/mm/yyyy format instead of yyyy-mm-dd
     arr[1] = date;
     arr[2] = `${field.currency}${arr[2]}`;
@@ -15,6 +18,9 @@ export function appendToTable(arr) {
         column.textContent = item;
         row.appendChild(column);
     }
+    let deleteColumn = document.createElement('td');
+    row.appendChild(deleteColumn);
+    deleteColumn.appendChild(button);
     form.reset();
     tableList.length = 0; // Clear array
 }
